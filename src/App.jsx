6a5d2d5336c62ecd555192a4a6c9087e30ee1bda
deepanshu121020 <deeps4829@gmail.com>
2024-07-30 11:10:15 +0530

    Resolved Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashborad';
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div>
